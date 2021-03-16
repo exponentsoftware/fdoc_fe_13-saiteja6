9467bc22d3c7d85841218bef33a38a98aba6071f
saiteja6 <bendalamsaiteja6@gmail.com>
2021-03-16 20:48:13 +0530

    Add a new AddAlbum component

--- music-streaming-8/src/components/AddAlbum.js
+++ music-streaming-8/src/components/AddAlbum.js
@@ -1,26 +1,38 @@
-// import React from 'react'
+import React from 'react'
 
-// export default function AddAlbum() {
-//     // const [state, setstate] = useState(initialState)
-//     // const [state, setstate] = useState(initialState)
-//     return (
-//         <div>
-//             <form>
-//                 <input
-//                     type='text'
-//                     placeholder='album_title'
-//                     value={}
-//                     onChange={}
-//                     >
-//                 </input>
-//                 <input
-//                     type='text'
-//                     placeholder='aritst'
-//                     value={}
-//                     onChange={}
-//                     >
-//                 </input>
-//             </form>
-//         </div>
-//     )
-// }
+export default class AddAlbum extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            album_title: '',
+            artist: ''
+        }
+    }
+
+    handleChange = (e) => {
+
+    }
+
+    render() {
+    return (
+        <div>
+            <form>
+                <input
+                    type='text'
+                    placeholder='album_title'
+                    value={this.state.album_title}
+                    onChange={this.handleChange}
+                    >
+                </input>
+                <input
+                    type='text'
+                    placeholder='aritst'
+                    value={this.state.artist}
+                    onChange={this.handleChange}
+                    >
+                </input>
+            </form>
+        </div>
+    )
+    }
+}
